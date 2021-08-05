b6f587a91b6b2b50b2d4d9a39b8c140c8bb96491
Sabrina Evans <sabrina.evans997@hotmail.co.uk>
2021-08-05 16:21:34 +0100

    Write gap function to handle standard cases

--- src/primeGap.spec.ts
+++ src/primeGap.spec.ts
@@ -7,12 +7,14 @@ test("tests whether a number is prime", () => {
 });
 
 test("calculates first two primes between m & n with given gap g", () => {
-  expect(gap(1, 1, 10)).toBe([1, 2]);
-  expect(gap(5, 2, 20)).toBe([2, 7]);
-  expect(gap(4, 3, 12)).toBe([7, 11]);
-  expect(gap(4, 100, 110)).toBe([103, 107]);
-  expect(gap(2, 100, 110)).toBe([101, 103]);
-  expect(gap(4, 100, 110)).toBe([103, 107]);
+  expect(gap(6, 3, 29)).toBe(null);
+  expect(gap(8, 1, 29)).toBe(null);
+  expect(gap(1, 1, 10)).toStrictEqual([1, 2]);
+  expect(gap(5, 2, 20)).toStrictEqual([2, 7]);
+  expect(gap(4, 3, 12)).toStrictEqual([7, 11]);
+  expect(gap(4, 100, 110)).toStrictEqual([103, 107]);
+  expect(gap(2, 100, 110)).toStrictEqual([101, 103]);
+  expect(gap(4, 100, 110)).toStrictEqual([103, 107]);
   expect(gap(7, 100, 110)).toBe(null);
-  expect(gap(8, 300, 400)).toBe([359, 367]);
+  expect(gap(8, 300, 400)).toStrictEqual([359, 367]);
 });
--- src/primeGap.ts
+++ src/primeGap.ts
@@ -8,7 +8,50 @@ function isPrime(x: number): boolean {
   return true;
 }
 
+function hasPrimeBetween(x: number, y: number): number | boolean {
+  let primeNumber: number | boolean = false;
+
+  for (let z = x + 2; z < y; z += 2) {
+    if (isPrime(z)) {
+      primeNumber = z;
+    }
+  }
+
+  return primeNumber;
+}
+
 function gap(g: number, m: number, n: number): number[] | null {
+  // rule out cases where g is even and m is greater than 2
+  if (g % 2 !== 0) {
+    if (2 < m || !isPrime(2 + g)) {
+      return null;
+    }
+
+    if (g === 1 && m <= 1) {
+      return [1, 2];
+    }
+
+    return [2, 2 + g];
+  }
+
+  // start at odd number
+  m = m % 2 === 1 ? m : m + 1;
+
+  // iterate through odd numbers checking if they're prime
+  for (let x = m; x <= n - g; x += 2) {
+    if (isPrime(x) && isPrime(x + g)) {
+      if (g === 2) {
+        return [x, x + g];
+      }
+
+      // check numbers in between
+      if (!hasPrimeBetween(x, x + g)) {
+        console.log(x, x + g);
+        return [x, x + g];
+      }
+    }
+  }
+
   return null;
 }
 
